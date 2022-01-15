bf516896390a6535f9284cfcce1a1beeaa4a2933
crazyfirey <86016971+crazyfirey@users.noreply.github.com>
2022-01-15 09:35:14 -0500

    first commit

---
+++ example_mods/stages/cafe.lua
@@ -0,0 +1,118 @@
+
+
+local u = false;
+local r = 0;
+local shot = false;
+local agent = 1
+local health = 0;
+local xx = 500;
+local yy = 400;
+local xx2 = 850;
+local yy2 = 500;
+local ofs = 50;
+local followchars = true;
+local del = 0;
+local del2 = 0;
+
+local angleshit = 1;
+local anglevar = 1;
+
+
+function onCreate()
+
+	makeAnimatedLuaSprite('cafe/Cafe', 'cafe/Cafe', -600, -300);
+	addAnimationByPrefix('cafe/Cafe', 'beat', 'cafe', 24, true);
+	addLuaSprite('cafe/Cafe', false);
+	objectPlayAnimation('cafe/Cafe', 'beat', true);
+	
+end
+
+function onUpdate()
+    if del > 0 then
+        del = del - 1
+    end
+    if del2 > 0 then
+        del2 = del2 - 1
+    end
+    if followchars == true then
+        if mustHitSection == false then
+            setProperty('defaultCamZoom',1)
+            if getProperty('dad.animation.curAnim.name') == 'singLEFT' then
+                triggerEvent('Camera Follow Pos',xx-ofs,yy)
+            end
+            if getProperty('dad.animation.curAnim.name') == 'singRIGHT' then
+                triggerEvent('Camera Follow Pos',xx+ofs,yy)
+            end
+            if getProperty('dad.animation.curAnim.name') == 'singUP' then
+                triggerEvent('Camera Follow Pos',xx,yy-ofs)
+            end
+            if getProperty('dad.animation.curAnim.name') == 'singDOWN' then
+                triggerEvent('Camera Follow Pos',xx,yy+ofs)
+            end
+            if getProperty('dad.animation.curAnim.name') == 'singLEFT-alt' then
+                triggerEvent('Camera Follow Pos',xx-ofs,yy)
+            end
+            if getProperty('dad.animation.curAnim.name') == 'singRIGHT-alt' then
+                triggerEvent('Camera Follow Pos',xx+ofs,yy)
+            end
+            if getProperty('dad.animation.curAnim.name') == 'singUP-alt' then
+                triggerEvent('Camera Follow Pos',xx,yy-ofs)
+            end
+            if getProperty('dad.animation.curAnim.name') == 'singDOWN-alt' then
+                triggerEvent('Camera Follow Pos',xx,yy+ofs)
+            end
+            if getProperty('dad.animation.curAnim.name') == 'idle-alt' then
+                triggerEvent('Camera Follow Pos',xx,yy)
+            end
+            if getProperty('dad.animation.curAnim.name') == 'idle' then
+                triggerEvent('Camera Follow Pos',xx,yy)
+            end
+        else
+
+            setProperty('defaultCamZoom',1)
+            if getProperty('boyfriend.animation.curAnim.name') == 'singLEFT' then
+                triggerEvent('Camera Follow Pos',xx2-ofs,yy2)
+            end
+            if getProperty('boyfriend.animation.curAnim.name') == 'singRIGHT' then
+                triggerEvent('Camera Follow Pos',xx2+ofs,yy2)
+            end
+            if getProperty('boyfriend.animation.curAnim.name') == 'singUP' then
+                triggerEvent('Camera Follow Pos',xx2,yy2-ofs)
+            end
+            if getProperty('boyfriend.animation.curAnim.name') == 'singDOWN' then
+                triggerEvent('Camera Follow Pos',xx2,yy2+ofs)
+            end
+            if getProperty('dad.animation.curAnim.name') == 'idle-alt' then
+                triggerEvent('Camera Follow Pos',xx2,yy2)
+            end
+            if getProperty('dad.animation.curAnim.name') == 'idle' then
+                triggerEvent('Camera Follow Pos',xx2,yy2)
+            end
+        end
+    else
+        triggerEvent('Camera Follow Pos','','')
+    end    
+end
+
+function onBeatHit()
+    
+    if curBeat > 32 then
+		triggerEvent('Add Camera Zoom', 0.04,0.05)
+
+		if curBeat % 2 == 0 then
+			angleshit = anglevar;
+		else
+			angleshit = -anglevar;
+		end
+		setProperty('camHUD.angle',angleshit*3)
+		setProperty('camGame.angle',angleshit*3)
+		doTweenAngle('turn', 'camHUD', angleshit, stepCrochet*0.002, 'circOut')
+		doTweenX('tuin', 'camHUD', -angleshit*8, crochet*0.001, 'linear')
+		doTweenAngle('tt', 'camGame', angleshit, stepCrochet*0.002, 'circOut')
+		doTweenX('ttrn', 'camGame', -angleshit*8, crochet*0.001, 'linear')
+	else
+		setProperty('camHUD.angle',0)
+		setProperty('camHUD.x',0)
+		setProperty('camHUD.x',0)
+	end
+end
---
+++ source/CreditsState.hx
@@ -0,0 +1,169 @@
+package;
+
+#if desktop
+import Discord.DiscordClient;
+#end
+import flash.text.TextField;
+import flixel.FlxG;
+import flixel.FlxSprite;
+import flixel.addons.display.FlxGridOverlay;
+import flixel.group.FlxGroup.FlxTypedGroup;
+import flixel.math.FlxMath;
+import flixel.text.FlxText;
+import flixel.util.FlxColor;
+import flixel.tweens.FlxTween;
+import lime.utils.Assets;
+
+using StringTools;
+
+class CreditsState extends MusicBeatState
+{
+	var curSelected:Int = 1;
+
+	private var grpOptions:FlxTypedGroup<Alphabet>;
+	private var iconArray:Array<AttachedSprite> = [];
+
+	private static var creditsStuff:Array<Dynamic> = [ //Name - Icon name - Description - Link - BG Color
+		['Novisor Dev Team Inc.'],
+		['crazyfirey',		    'crazyfirey',		'Director, Main Programmer, Main Charter',			    'https://bit.ly/3tilfT1',	            0xFFFB6D],
+		['GoodEggrollz',		'goodeggrollz',		'Artist/Animator & Composer',					        'https://bit.ly/3JWQRUh',	            0xFFFB6D],
+		['Frostish',		    'frostish',		    'Co-Charter',					                        'https://gamejolt.com/@Frostish',	    0xFFFB6D],
+		['Jamuel',              'jamuel',           'Co-Programmer',                                        'https://bit.ly/34wekew',               0xFFFB6D],
+		['Special Thanks'],
+	    ['Shadow Mario',		'shadowmario',		'Created Psych Engine',					                'https://gamebanana.com/members/1735892',	    0xFFFB6D],
+	    ['GameToons',		    'gametoons',		'Created Novisor Series',					            'https://www.tiktok.com/@gametoonsofficial',	0xFFFB6D]
+	];
+
+	var bg:FlxSprite;
+	var descText:FlxText;
+	var intendedColor:Int;
+	var colorTween:FlxTween;
+
+	override function create()
+	{
+		#if desktop
+		// Updating Discord Rich Presence
+		DiscordClient.changePresence("In the Menus", null);
+		#end
+
+		bg = new FlxSprite().loadGraphic(Paths.image('menuDesat'));
+		add(bg);
+
+		grpOptions = new FlxTypedGroup<Alphabet>();
+		add(grpOptions);
+
+		for (i in 0...creditsStuff.length)
+		{
+			var isSelectable:Bool = !unselectableCheck(i);
+			var optionText:Alphabet = new Alphabet(0, 70 * i, creditsStuff[i][0], !isSelectable, false);
+			optionText.isMenuItem = true;
+			optionText.screenCenter(X);
+			if(isSelectable) {
+				optionText.x -= 70;
+			}
+			optionText.forceX = optionText.x;
+			//optionText.yMult = 90;
+			optionText.targetY = i;
+			grpOptions.add(optionText);
+
+			if(isSelectable) {
+				var icon:AttachedSprite = new AttachedSprite('credits/' + creditsStuff[i][1]);
+				icon.xAdd = optionText.width + 10;
+				icon.sprTracker = optionText;
+	
+				// using a FlxGroup is too much fuss!
+				iconArray.push(icon);
+				add(icon);
+			}
+		}
+
+		descText = new FlxText(50, 600, 1180, "", 32);
+		descText.setFormat(Paths.font("vcr.ttf"), 32, FlxColor.WHITE, CENTER, FlxTextBorderStyle.OUTLINE, FlxColor.BLACK);
+		descText.scrollFactor.set();
+		descText.borderSize = 2.4;
+		add(descText);
+
+		bg.color = creditsStuff[curSelected][4];
+		intendedColor = bg.color;
+		changeSelection();
+		super.create();
+	}
+
+	override function update(elapsed:Float)
+	{
+		if (FlxG.sound.music.volume < 0.7)
+		{
+			FlxG.sound.music.volume += 0.5 * FlxG.elapsed;
+		}
+
+		var upP = controls.UI_UP_P;
+		var downP = controls.UI_DOWN_P;
+
+		if (upP)
+		{
+			changeSelection(-1);
+		}
+		if (downP)
+		{
+			changeSelection(1);
+		}
+
+		if (controls.BACK)
+		{
+			if(colorTween != null) {
+				colorTween.cancel();
+			}
+			FlxG.sound.play(Paths.sound('cancelMenu'));
+			MusicBeatState.switchState(new MainMenuState());
+		}
+		if(controls.ACCEPT) {
+			CoolUtil.browserLoad(creditsStuff[curSelected][3]);
+		}
+		super.update(elapsed);
+	}
+
+	function changeSelection(change:Int = 0)
+	{
+		FlxG.sound.play(Paths.sound('scrollMenu'), 0.4);
+		do {
+			curSelected += change;
+			if (curSelected < 0)
+				curSelected = creditsStuff.length - 1;
+			if (curSelected >= creditsStuff.length)
+				curSelected = 0;
+		} while(unselectableCheck(curSelected));
+
+		var newColor:Int = creditsStuff[curSelected][4];
+		if(newColor != intendedColor) {
+			if(colorTween != null) {
+				colorTween.cancel();
+			}
+			intendedColor = newColor;
+			colorTween = FlxTween.color(bg, 1, bg.color, intendedColor, {
+				onComplete: function(twn:FlxTween) {
+					colorTween = null;
+				}
+			});
+		}
+
+		var bullShit:Int = 0;
+
+		for (item in grpOptions.members)
+		{
+			item.targetY = bullShit - curSelected;
+			bullShit++;
+
+			if(!unselectableCheck(bullShit-1)) {
+				item.alpha = 0.6;
+				if (item.targetY == 0) {
+					item.alpha = 1;
+				}
+			}
+		}
+		descText.text = creditsStuff[curSelected][2];
+	}
+
+	private function unselectableCheck(num:Int):Bool {
+		return creditsStuff[num].length <= 1;
+	}
+}
